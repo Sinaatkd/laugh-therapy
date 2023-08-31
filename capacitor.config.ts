0a2083b1484bfe49dad5386551a9a898b8caf68e
Sina Amini <general.sin.amini.20@gmail.com>
2023-08-31 19:32:43 +0330

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'laugh-therapy',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
