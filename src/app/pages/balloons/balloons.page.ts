e53afa189096365f264c6098633bda55a4659348
Sina Amini <general.sin.amini.20@gmail.com>
2023-09-02 11:29:19 +0330

    balloons transfer from bottom of screen to top and when clicked on balloon, balloon burs happend and sound effect played

--- src/app/pages/balloons/balloons.page.ts
+++ src/app/pages/balloons/balloons.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-balloons',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./balloons.page.scss'],
 })
 export class BalloonsPage implements OnInit {
+  balloonsIndex = [...Array(environment.BALLOON_COUNT).keys()];
+  fullBalloonIndex = Math.floor(Math.random() * environment.BALLOON_COUNT)
+  counter = 0;
 
-  constructor() { }
+  constructor(
+    private alertCtrl: AlertController
+  ) {
+  }
 
   ngOnInit() {
   }
 
+  balloonBurst(balloonEl: HTMLElement, balloonIndex: number) {
+    if (balloonEl.style.opacity !== '0') {
+      const audio = new Audio('../../../assets/sounds/sounds-effect/Balloon-Blast-Sound-Effect.mp3');
+      audio.play().then();
+      this.counter += 1;
+      balloonEl.style.opacity = '0';
+      if (balloonIndex == this.fullBalloonIndex) {
+        
+      }
+    }
+  }
 }
