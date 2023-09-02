acf20f16716c0e07a771767cbbbe1b03a1278add
Sina Amini <general.sin.amini.20@gmail.com>
2023-09-02 17:31:44 +0330

    go back when button clicked and play Win-Effect.mp3

--- src/app/components/balloon-win/balloon-win.component.ts
+++ src/app/components/balloon-win/balloon-win.component.ts
@@ -6,23 +6,27 @@ import { ModalController, NavController } from '@ionic/angular';
   templateUrl: './balloon-win.component.html',
   styleUrls: ['./balloon-win.component.scss'],
 })
-export class BalloonWinComponent  implements OnInit {
+export class BalloonWinComponent implements OnInit {
 
   @Input('counter') counter?: number;
   randomImageNumber = Math.floor(Math.random() * 10);
   imageAddress = '../../../assets/images/cartoon-character/'
   randomImageName = `${this.randomImageNumber}.png`;
-  
+  audio = new Audio('../../../assets/sounds/sounds-effect/Win-Effect.mp3');
+
   constructor(
     private navCtrl: NavController,
     private modalCtrl: ModalController
   ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.audio.play();
+  }
 
   backToHomePage() {
     this.modalCtrl.dismiss().then(_ => {
       this.navCtrl.navigateBack('/').then();
+      this.audio.pause();
     })
   }
 }
