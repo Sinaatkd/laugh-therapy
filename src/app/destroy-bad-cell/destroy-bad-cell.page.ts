57ce3f62a88a048e3837714f92e219f820071e93
Sina Amini <general.sin.amini.20@gmail.com>
2023-09-06 18:00:44 +0330

    cells destroyed when the bullet collides with it

--- src/app/destroy-bad-cell/destroy-bad-cell.page.ts
+++ src/app/destroy-bad-cell/destroy-bad-cell.page.ts
@@ -10,10 +10,10 @@ export class DestroyBadCellPage implements OnInit {
 
   // 0: means bad cell and 1 means good cell and 2 means destroyed
   characters: number[] = []
+  deadCharacters: number[] = [];
 
   bullet = 10;
   isStartBlank = true;
-  isEndGame = false;
 
   constructor() { }
 
@@ -31,7 +31,7 @@ export class DestroyBadCellPage implements OnInit {
   ionViewWillLeave() {
     clearInterval(this.checkCharactersXIntervalID)
   }
-  
+
   ngOnInit() {
     this.characters = this.createRandomCharacter(16)
   }
@@ -53,14 +53,58 @@ export class DestroyBadCellPage implements OnInit {
 
   shoot() {
     if (this.bullet > 0) {
-      const spaceshipElRect = document.getElementById('spaceshipEl')?.getBoundingClientRect()!;
-      const newBullet = document.createElement('div');
-      newBullet.className = 'bullet';
-      newBullet.style.left = spaceshipElRect.x + 22 + 'px'
-      newBullet.style.top = spaceshipElRect.y - 15 + 'px'
-      const ionContent = document.getElementById('ionContent');
-      ionContent?.appendChild(newBullet);
       this.bullet -= 1;
+      const newBullet = this.createBullet();
+      let checkTimes = 0;
+      setTimeout(() => {
+        this.checkBulletCollision(newBullet, checkTimes);
+      }, 50)
+    } 
+  }
+
+  createBullet() {
+    const spaceshipElRect = document.getElementById('spaceshipEl')?.getBoundingClientRect()!;
+    const newBullet = document.createElement('div');
+    newBullet.className = 'bullet';
+    newBullet.style.left = spaceshipElRect.x + 22 + 'px'
+    newBullet.style.top = spaceshipElRect.y - 15 + 'px'
+    const ionContent = document.getElementById('ionContent');
+    ionContent?.appendChild(newBullet);
+    newBullet.onanimationend = (en) => {
+      ionContent?.removeChild(newBullet);
+      newBullet.remove();
+    }
+    return newBullet;
+  }
+
+  checkBulletCollision(bullet: HTMLDivElement, checkTime: number) {
+    if (checkTime <= 14) {
+      const bulletRect = bullet.getBoundingClientRect()!;
+      const characters = document.getElementsByTagName('ion-img');
+      for (let i = 0; i < characters.length; i++) {
+        const character = characters[i].getBoundingClientRect()!;
+        if (
+          bulletRect.right >= character.left &&
+          bulletRect.left <= character.right &&
+          bulletRect.bottom >= character.top &&
+          bulletRect.top <= character.bottom
+        ) {
+          bullet.remove();
+          if (characters[i].src?.toString().indexOf('good') == -1) {
+            this.bullet += 1;
+            this.deadCharacters.push(0);
+          } else {
+            this.bullet -= 1;
+            this.deadCharacters.push(1);
+          }
+          characters[i].remove();
+          return;
+        }
+      }
+      checkTime += 1;
+      setTimeout(() => {
+        this.checkBulletCollision(bullet, checkTime)
+      }, 50);
     }
   }
 
