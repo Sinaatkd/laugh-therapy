b12c2ade9787fed1c326bf83438cd6cbe445d64b
Sina Amini <general.sin.amini.20@gmail.com>
2023-09-15 01:24:47 +0330

    tired-character, changed-character-style folders renamed to tired-dannys, change-dannys-style

--- src/app/components/shared.module.ts
+++ src/app/components/shared.module.ts
@@ -6,11 +6,11 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
 import { GameItemComponent } from './game-item/game-item.component';
 import { RouterModule } from '@angular/router';
 import { BalloonWinComponent } from './balloon-win/balloon-win.component';
-import { ChangeDannysStyleComponent } from './change-character-style/change-dannys-style.component';
+import { ChangeDannysStyleComponent } from './change-dannys-style/change-dannys-style.component';
 import { DannysHat1Component } from './dannys-hat1/dannys-hat1.component';
 import { DannysHat2Component } from './dannys-hat2/dannys-hat2.component';
 import { DannysHair1Component } from './dannys-hair1/dannys-hair1.component';
-import { TiredDannysComponent } from './tired-character/tired-dannys.component';
+import { TiredDannysComponent } from './tired-dannys/tired-dannys.component';
 import { DannysFoodComponent } from './dannys-food/dannys-food.component';
 
 
