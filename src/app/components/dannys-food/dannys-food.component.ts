7c4838a8ddd6c33142672385409f0cbe18eee57c
Sina Amini <general.sin.amini.20@gmail.com>
2023-09-15 15:24:09 +0330

    dannys-food component design completed, show foods and show dannys food have

--- src/app/components/dannys-food/dannys-food.component.ts
+++ src/app/components/dannys-food/dannys-food.component.ts
@@ -5,10 +5,75 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dannys-food.component.html',
   styleUrls: ['./dannys-food.component.scss'],
 })
-export class DannysFoodComponent  implements OnInit {
+export class DannysFoodComponent implements OnInit {
+
+  showDannysFoodHave = false;
+
+  foods = [
+    {
+      id: 1,
+      title: 'میوه',
+      image: '../../../assets/images/dannys-food/fruit.png',
+      elementId: 'fruit',
+    },
+    {
+      id: 2,
+      title: 'آبمیوه',
+      image: '../../../assets/images/dannys-food/juice.png',
+      elementId: 'juice',
+    },
+    {
+      id: 3,
+      title: 'تخم مرغ',
+      image: '../../../assets/images/dannys-food/fried-egg.png',
+      elementId: 'fried-egg',
+    },
+    {
+      id: 4,
+      title: 'سوپ',
+      image: '../../../assets/images/dannys-food/soup.png',
+      elementId: 'soup',
+    },
+    {
+      id: 5,
+      title: 'فرنی',
+      image: '../../../assets/images/dannys-food/porridge.png',
+      elementId: 'porridge',
+    },
+    {
+      id: 6,
+      title: 'ماهی',
+      image: '../../../assets/images/dannys-food/fried-fish.png',
+      elementId: 'fried-fish',
+    },
+    {
+      id: 7,
+      title: 'شیر',
+      image: '../../../assets/images/dannys-food/milk.png',
+      elementId: 'milk',
+    },
+  ]
+
+  selectedFood = this.choiceRandomFood();
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.setShowDannysFoodHave();
+  }
+
+  choiceRandomFood(): any {
+    const randomId = Math.floor(Math.random() * (7 - 1 + 1)) + 1;
+    return this.foods.filter(food => food.id === randomId)[0]
+  }
+
+  setShowDannysFoodHave() {
+    if (!this.showDannysFoodHave) {
+      this.showDannysFoodHave = true
+      setTimeout(() => {
+        this.showDannysFoodHave = false;
+      }, 5000);
+    }
+  }
 
 }
