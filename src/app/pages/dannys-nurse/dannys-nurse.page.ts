fb0f1916e9c4fdbe54ed71d007b7f56e0863e75b
Sina Amini <general.sin.amini.20@gmail.com>
2023-09-16 23:41:52 +0330

    select random danny game and show win alert when done event called

--- src/app/pages/dannys-nurse/dannys-nurse.page.ts
+++ src/app/pages/dannys-nurse/dannys-nurse.page.ts
@@ -1,7 +1,6 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
-import html2canvas from 'html2canvas';
-
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-dannys-nurse',
@@ -10,12 +9,55 @@ import html2canvas from 'html2canvas';
 })
 export class DannysNursePage implements OnInit {
   constructor(
-    @Inject(DOCUMENT) document: Document
+    @Inject(DOCUMENT) document: Document,
+    private alertCtrl: AlertController,
+    private navCtrl: NavController,
   ) {
     document.dir = 'ltr';
   }
 
-  ngOnInit(): void {
-    
+  gameNumber = this.getRandomGameNumber(1, 4);
+
+  ionViewDidLeave() {
+    document.dir = 'rtl';
+  }
+
+  ngOnInit(): void { }
+
+  getRandomGameNumber(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  done(event: any) {
+    const soundAddress = '../../../assets/sounds/sounds-effect/Win-Effect.mp3';
+    const audio = new Audio(soundAddress)
+    this.alertCtrl.create({
+      message: 'تو تونستی دنی رو خوشحال کنی 🥳🥳',
+      mode: 'ios',
+      backdropDismiss: false,
+      buttons: [
+        {
+          text: 'ادامه',
+          handler: () => {
+            this.resetGame();
+            audio.pause();
+          }
+        },
+        {
+          text: 'بازگشت به صفحه اصلی',
+          handler: () => {
+            this.navCtrl.navigateBack('/home');
+            audio.pause();
+          }
+        }
+      ]
+    }).then(alertEl => alertEl.present());
+  }
+
+  resetGame() {
+    const oldGameNumber = this.gameNumber;
+    while (oldGameNumber === this.gameNumber) {
+      this.gameNumber = this.getRandomGameNumber(1, 4);
+    }
   }
 }
