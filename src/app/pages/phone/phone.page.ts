61822eca125a966fa3294f9748935456afe61729
Sina Amini <general.sin.amini.20@gmail.com>
2023-09-24 21:21:33 +0330

    v1.1 beta

--- src/app/pages/phone/phone.page.ts
+++ src/app/pages/phone/phone.page.ts
@@ -1,18 +1,82 @@
-import { DOCUMENT } from '@angular/common';
-import { Component, Inject, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { CallComponent } from 'src/app/components/call/call.component';
+import {
+  DOCUMENT
+}
+  from '@angular/common';
+import {
+  Component,
+  Inject,
+  OnInit
+}
+  from '@angular/core';
+import {
+  ModalController
+}
+  from '@ionic/angular';
+import {
+  CallComponent
+}
+  from 'src/app/components/call/call.component';
 
 @Component({
   selector: 'app-phone',
   templateUrl: './phone.page.html',
   styleUrls: ['./phone.page.scss'],
-})
+}
+)
 export class PhonePage implements OnInit {
 
 
   text = '';
-  keys = ['3', '2', '1', '6', '5', '4', '9', '8', '7', '*', '0', '#']
+  keys = [
+    {
+      key: '3',
+
+    },
+    {
+      key: '2',
+
+    },
+    {
+      key: '1',
+
+    },
+    {
+      key: '6',
+
+    },
+    {
+      key: '5',
+
+    },
+    {
+      key: '4',
+
+    },
+    {
+      key: '9',
+
+    },
+    {
+      key: '8',
+
+    },
+    {
+      key: '7',
+
+    },
+    {
+      key: '*',
+
+    },
+    {
+      key: '0',
+
+    },
+    {
+      key: '#',
+    }
+
+  ]
 
   constructor(
     @Inject(DOCUMENT) document: Document,
@@ -21,39 +85,52 @@ export class PhonePage implements OnInit {
     document.dir = 'ltr';
   }
 
+
   ionViewDidEnter() {
     this.call()
   }
 
+
   call() {
     setTimeout(() => {
       this.openCallModal();
-    }, 6000);
+    }
+      ,
+      6000);
   }
+
 
   ionViewDidLeave() {
     document.dir = 'rtl';
   }
 
+
   ngOnInit() {
   }
+
 
   appendCharacter2Text(char: string) {
     this.text += char;
   }
 
+
   clearText() {
     this.text = '';
   }
 
+
   openCallModal() {
     this.modalCtrl.create({
       component: CallComponent,
-    }).then(modalEl => {
+    }
+    ).then(modalEl => {
       modalEl.present();
       modalEl.onDidDismiss().then(() => {
         this.call();
-      })
-    });
+      }
+      )
+    }
+    );
   }
 }
+
